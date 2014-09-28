c11024e2e946970d4b0541a100cdde69c4dcac9c
rexim <reximkut@gmail.com>
2014-09-28 22:58:12 +0700

    Parsing quote ids manually. Close #12.

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,29 +1,23 @@
 package controllers
 
-import java.sql.Timestamp
-import javax.sql.DataSource
-
 import data.Data
-import play.api._
+import helpers.TypeHelpers._
 import play.api.Play.current
 import play.api.db.DB
 import play.api.mvc._
 
-object UserController extends Controller {
 
+object UserController extends Controller {
   implicit def dataSource = DB.getDataSource()
 
   def index = Action {
     Ok(views.html.index(Data.getAllQuotes))
   }
 
-  def quote(id: Long) = Action { implicit request =>
-    val quote = Data.getQuoteById(id)
-    if(quote.isDefined)
-      Ok(views.html.quote(quote))
-    else
-      NotFound("Not found")
+  def quote(idString: String) = Action { implicit request =>
+    parseLong(idString).flatMap(Data.getQuoteById) match {
+      case Some(quote) => Ok(views.html.quote(quote))
+      case _           => NotFound("Not found")
+    }
   }
-
-
 }
