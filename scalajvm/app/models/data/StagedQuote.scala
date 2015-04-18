21ddfb98940afe6b4f67afef51bc2c28ad9fb312
rexim <reximkut@gmail.com>
2015-04-18 16:34:48 +0600

    Optional Stager IP (#100)

--- scalajvm/app/models/data/StagedQuote.scala
+++ scalajvm/app/models/data/StagedQuote.scala
@@ -3,11 +3,11 @@ package models.data
 import org.joda.time.DateTime
 import scalikejdbc._
 
-case class StagedQuote(id: Long, token: String, content: String, time: DateTime, stagerIp: String)
+case class StagedQuote(id: Long, token: String, content: String, time: DateTime, stagerIp: Option[String])
 object StagedQuote extends SQLSyntaxSupport[StagedQuote] {
   override val tableName = "staged_quote"
   def apply(rs: WrappedResultSet) =
     new StagedQuote(rs.long("id"), rs.string("token"),
       rs.string("content"), rs.jodaDateTime("time"),
-      rs.string("stager_ip"))
+      rs.stringOpt("stager_ip"))
 }
--- scalajvm/conf/evolutions/default/8.sql
+++ scalajvm/conf/evolutions/default/8.sql
@@ -4,14 +4,14 @@ create table if not exists staged_quote (
   token varchar(32) not null default encode(gen_random_bytes(16), 'hex'),
   content varchar not null,
   time timestamp not null,
-  stager_ip varchar not null
+  stager_ip varchar
 );
 
 create table if not exists maintainer (
   id serial primary key,
   name varchar not null,
   email varchar not null
-)
+);
 
 # --- !Downs
 drop table if exists maintainer;
