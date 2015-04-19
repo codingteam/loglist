2a5541cecf737091daa0636d2e4ac978a1aaf476
rexim <reximkut@gmail.com>
2015-04-19 13:58:57 +0600

    Enable PostgreSQL for Travis. Close #104

---
+++ scalajvm/test/ApproverModelSpec.scala
@@ -0,0 +1,27 @@
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+import scalikejdbc._
+
+class ApproverModelSpec extends Specification {
+  val approvers = List(
+    ("rexim", "rexim@loglist.net"),
+    ("ForNeVeR", "fornever@loglist.net")
+  )
+
+  "The Approvers model" should {
+    "be able to add new approvers and return them back" in {
+      running(FakeApplication()) {
+        DB localTx { implicit session =>
+          for ((name, email) <- approvers) {
+            models.queries.ApproverQueries().insertApprover(name, email)
+          }
+          models.queries.ApproverQueries().getAllApprovers must have size approvers.size
+        }
+      }
+
+    }
+  }
+}
