28d35249fce765fe64894ef335a47630dbf5dffd
Alexander Batischev <eual.jp@gmail.com>
2016-03-30 20:17:03 +0300

    Stop explicitly handling CDATA in `getRSSFeed`

--- scalajvm/app/models/data/FeedItem.scala
+++ scalajvm/app/models/data/FeedItem.scala
@@ -3,4 +3,4 @@ package models.data
 import org.joda.time.DateTime
 import scala.xml._
 
-case class FeedItem(time: DateTime, title: scala.xml.Atom[String], description: scala.xml.Atom[String], link: String, guid: String)
+case class FeedItem(time: DateTime, title: String, description: String, link: String, guid: String)
