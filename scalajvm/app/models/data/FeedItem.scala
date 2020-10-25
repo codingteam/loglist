3a4d73bfec3f059c3729fd8b9f0010eef646beea
Friedrich von Never <friedrich@fornever.me>
2020-10-25 17:39:00 +0700

    #226: time formatting improvements

--- scalajvm/app/models/data/FeedItem.scala
+++ scalajvm/app/models/data/FeedItem.scala
@@ -1,5 +1,10 @@
 package models.data
 
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
-case class FeedItem(time: ZonedDateTime, title: String, description: String, link: String, guid: String)
+case class FeedItem(time: ZonedDateTime, title: String, description: String, link: String, guid: String) {
+  def rfcFormattedTime: String = {
+    time.format(DateTimeFormatter.RFC_1123_DATE_TIME)
+  }
+}
--- scalajvm/app/models/data/Quote.scala
+++ scalajvm/app/models/data/Quote.scala
@@ -1,10 +1,15 @@
 package models.data
 
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 import scalikejdbc._
 
-case class Quote(id: Long, source: String, sourceUrl: Option[String], time: ZonedDateTime, content: Option[String], rating: Int)
+case class Quote(id: Long, source: String, sourceUrl: Option[String], time: ZonedDateTime, content: Option[String], rating: Int) {
+  def formattedTime: String =
+    time.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+}
+
 object Quote extends SQLSyntaxSupport[Quote] {
   override val tableName = "quote"
 
