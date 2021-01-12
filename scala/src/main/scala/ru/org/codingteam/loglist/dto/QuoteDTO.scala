dac6c412013fcc43642d9ada061490f30c724bd9
Friedrich von Never <friedrich@fornever.me>
2021-01-13 01:47:50 +0700

    Fix #235: use Option instead of direct nulls in the quote API

--- scala/src/main/scala/ru/org/codingteam/loglist/dto/QuoteDTO.scala
+++ scala/src/main/scala/ru/org/codingteam/loglist/dto/QuoteDTO.scala
@@ -1,3 +1,3 @@
 package ru.org.codingteam.loglist.dto
 
-case class QuoteDTO(id: Long, source: String, sourceUrl: String, time: Long, content: String, rating: Int)
+case class QuoteDTO(id: Long, source: String, sourceUrl: Option[String], time: Long, content: String, rating: Int)
